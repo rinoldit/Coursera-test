52772e8564ec9cd8f3e8f8aea24a91bad2b43cf9
rinold.s <rinold.s@raster.in>
2017-09-23 11:15:00 +0530

    DELETED FILES OR RELOCATED FILES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { HeroDetailsComponent } from './/hero-details/hero-details.component';
-import { HeroService} from'./service/hero.service';
-import {HeroesComponent} from './heroes/heroes.component';
-import { RouterModule }   from '@angular/router';
-import { PracticeBasic1Component } from './practice-basic1/practice-basic1.component';
+
 import { ReactiveFormsModule } from '@angular/forms';
+import { HamBurgerComponent } from '../app/ham-burger/ham-burger.component';
 
 @NgModule({
   declarations: [
       AppComponent,
-      HeroDetailsComponent,
-      HeroesComponent,
-      PracticeBasic1Component
+      HamBurgerComponent,
+     
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      }
-    ])
+    
 
   ],
-  providers: [HeroService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 
